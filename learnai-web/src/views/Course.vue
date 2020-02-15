1bb38aaaa56a9c2da3a56d745d0b909479846b47
Aayush Joglekar <aayushjog@gmail.com>
2020-02-16 03:54:45 +0530

    education: created basic layout

--- learnai-web/src/views/Course.vue
+++ learnai-web/src/views/Course.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="app-course">
     <div class="course-content">
-
+      <div class="lesson-pane">
+        Lessons
+      </div>
+      <div class="extra-pane">
+        Scam
+      </div>
     </div>
   </div>
 </template>
@@ -13,21 +18,37 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.app-course {
+  display: flex;
+  justify-content: center;
 
-  .app-course {
+  .course-content {
+    margin-top: 3rem;
+    background-color: black;
+    width: 65%;
 
     display: flex;
     justify-content: center;
 
-    .course-content {
-
-      padding: 5rem;
-      background-color: black;
-      width: 65%;
-
-    }
-
-
   }
 
+  .lesson-pane {
+    background-color: #eeeeee;
+    padding: 4rem;
+    flex: 0 0 60%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .extra-pane {
+    background-color: #e1e1e1;
+    padding: 4rem;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+}
 </style>
